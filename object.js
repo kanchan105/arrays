5838c23f0c2adf0fcfa7f47adb19d19df0cac97e
kanchan105 <kanchan5101997@gmail.com>
2023-10-05 19:32:08 -0500

    first commit

---
+++ object.js
@@ -0,0 +1,80 @@
+const k={
+  name:"kanchan",
+  age:25
+}
+console.log(k)
+
+const i={
+  name:"rana",
+  age:26
+}
+
+let num1=k.name+i.name
+console.log(num)
+let num = 255;
+console.log("hello")
+
+console.log( 255..toString(16) );
+let guestList = `Guests:
+ * John
+ * Pete
+ * Mary
+`;
+
+console.log(guestList);
+let str2 = `Hello`;
+
+alert( str[-2] ); // undefined
+alert( str.at(-2) ); // l
+
+let str1 = 'My name is kanchan';
+
+console.log( str.indexOf('kanchan',3 ) ) 
+let str="kanchan"
+console.log(str.slice(3,4))
+
+let arr=[1,2,3,4,5,"kanchan","rana",
+{
+  name:"virat"
+},
+function abc()
+{
+  console.log("kohli")
+}
+]
+arr[3]="happy"
+// console.log(arr[3])
+// console.log(arr.length)
+
+console.log(arr[7].name)
+arr[8]();
+
+let x=[211,2,3,4,3,4,2,4,111]
+console.log(x)
+console.log(x.unshift(211))
+console.log(x)
+
+const rows=10;
+const column=20;
+const arr=[]
+for(var ii=0;i<rows;i++)
+{
+  arr[i]=[]
+   for (var j=0;j<column;j++)
+   {
+    arr[i][j]=null
+    console.log(arr[i][j])
+   
+   }
+  }
+  
+var array=[11,12,13,14,15,16,17,18,19,20,21];
+// console.log(array.splice(5,3));
+const c={
+  name:"kanchan",
+  lname:"rana"
+}
+ console.log(array.concat(c))
+
+
+
